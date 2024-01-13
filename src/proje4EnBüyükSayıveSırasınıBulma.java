b69fa3af6e7b7bd9395c7c70a911cb0363bb8291
ErenCanErdoğan <107648911+eren6034@users.noreply.github.com>
2024-01-13 17:10:49 +0300

    Update proje4EnBüyükSayıveSırasınıBulma.java

--- src/proje4EnBüyükSayıveSırasınıBulma.java
+++ src/proje4EnBüyükSayıveSırasınıBulma.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class proje4EnBüyükSayıveSırasınıBulma {
+import java.util.Scanner;
+
+public class proje4EnBüyükSayıveSırasınıBulma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int enBuyukSayi = Integer.MIN_VALUE;
+        int sira = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + ". sayıyı girin: ");
+            int sayi = scanner.nextInt();
+
+            if (sayi > enBuyukSayi) {
+                enBuyukSayi = sayi;
+                sira = i;
+            }
+        }
+
+        System.out.println("En büyük sayı: " + enBuyukSayi);
+        System.out.println("Girildiği sıra: " + sira);
+
+        scanner.close();
+    }
 }
